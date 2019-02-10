c911c7486e92ab1f9d5e468b3aea616c68fa1f76
Ryan Barrett <ryanbarrett@Ryans-MacBook-Air-4.local>
2019-02-10 18:41:51 -0500

    User and Item model added with some validations

---
+++ Dronuts/common/models/item.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = function(Item) {
+    //not checking for url safeness yet
+    Item.validatesUniquenessOf('item_id');
+    Item.validatesUniquenessOf('name');
+    Item.valitadesLengthOf('name', {max: 80});
+    Item.validatesNumericalityOf('price', {int: false});
+    Item.validatesNumericalityOf('quanitiy_in_stock', {int: true});
+};
