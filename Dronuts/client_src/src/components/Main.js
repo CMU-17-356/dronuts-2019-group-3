6ee376d734e110cc6266eef044d73b328bc25a7e
derekyyyan <dyyan@andrew.cmu.edu>
2019-02-11 18:58:34 -0500

    added about us and locations

--- Dronuts/client_src/src/components/Main.js
+++ Dronuts/client_src/src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import About from './About';
+import Locations from './Locations';
 import Menu from './Menu';
 import Order from './Order';
 import Test from './Test';
@@ -8,6 +10,8 @@ const Main = () => (
   <main>
     <Switch>
       <Route exact path='/' component={Test} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/locations' component={Locations} />
       <Route exact path='/menu' component={Menu} />
       <Route exact path='/order' component={Order} />
     </Switch>
