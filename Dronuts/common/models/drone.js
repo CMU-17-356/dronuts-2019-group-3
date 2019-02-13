7ec3e2ae95c1d9261de063ff234b666d184811a3
judyzhang25 <31805219+judyzhang25@users.noreply.github.com>
2019-02-12 19:33:00 -0500

    removed tests, will be added later -judy

--- Dronuts/common/models/drone.js
+++ Dronuts/common/models/drone.js
@@ -1,13 +1,16 @@
-// 'use strict';
+'use strict';
+var app = require('../../server/server');
 
-// module.exports = function(Drone) {
-
-// };
-var dronesSchema = {
-    'drone_id': {
-        'type': 'string',
-        'required': true
-    }
+module.exports = function(Drone) {
+    Drone.validatesPresenceOf('drone_id');
+    Drone.validatesUniquenessOf('drone_id');
 };
 
-Model.extend('Drone', dronesSchema);
+// var dronesSchema = {
+//     'drone_id': {
+//         'type': 'string',
+//         'required': true
+//     }
+// };
+
+// Model.extend('Drone', dronesSchema);
--- Dronuts/test/drones.test.js
+++ Dronuts/test/drones.test.js
@@ -1,25 +1,25 @@
-require('jest-plugins')(['loopback-jest']);
-var loopback = require('loopback');
+// require('jest-plugins')(['loopback-jest']);
+// var loopback = require('loopback');
 
-describe('basic drone test', () => {
-    test('drone exists and has properties', ( ) => {
-        const drone1 = loopback.createModel({
-            name: 'Drone',
-            properties: {
-                drone_id: 'string'
-            }
-        });
-        expect(drone1).not.toBeNull();
-        expect(drone1).toHavePropertyOfType('drone_id', String);
-        expect(drone1).not.toHavePropertyOfType('drone_id', Number);
-        expect(drone1.drone_id).not.toBeNull();
+// describe('basic drone test', () => {
+//     test('drone exists and has properties', ( ) => {
+//         const drone1 = loopback.createModel({
+//             name: 'Drone',
+//             properties: {
+//                 drone_id: 'string'
+//             }
+//         });
+//         expect(drone1).not.toBeNull();
+//         expect(drone1).toHavePropertyOfType('drone_id', String);
+//         expect(drone1).not.toHavePropertyOfType('drone_id', Number);
+//         expect(drone1.drone_id).not.toBeNull();
 
-        const drone2 = loopback.createModel({
-            name: 'Drone',
-            properties: {
-                drone_id: 1
-            }
-        });
-        expect(drone2.drone_id).toBeUndefined();
-    });
-});
+//         const drone2 = loopback.createModel({
+//             name: 'Drone',
+//             properties: {
+//                 drone_id: 1
+//             }
+//         });
+//         expect(drone2.drone_id).toBeUndefined();
+//     });
+// });
