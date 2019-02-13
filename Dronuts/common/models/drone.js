599875f08b605380ae4cf7bffda9815baf42ceeb
judyzhang25 <31805219+judyzhang25@users.noreply.github.com>
2019-02-12 19:47:52 -0500

    consider the order and drone tests dummy tests, need to fix later, added model validations -judy

--- Dronuts/common/models/drone.js
+++ Dronuts/common/models/drone.js
@@ -1,5 +1,5 @@
 'use strict';
-var app = require('../../server/server');
+// var app = require('../../server/server');
 
 module.exports = function(Drone) {
     Drone.validatesPresenceOf('drone_id');
--- Dronuts/test/orders.test.js
+++ Dronuts/test/orders.test.js
@@ -1,37 +1,32 @@
-// require('jest-plugins')(['loopback-jest']);
-// // var loopback = require('loopback');
-// var app = require('../server/server');
+require('jest-plugins')(['loopback-jest']);
+var loopback = require('loopback');
+const order1 = loopback.createModel({
+    name: 'Order',
+    properties: {
+        order_id: 'string',
+        user_id: 'string',
+        drone_id: 'string',
+        items: ['string'],
+        destination_lat: 'number',
+        destination_lon: 'number',
+        completion_status: 'enum'
+    }
+});
 
-// var Order = app.models.Order;
-// const order1 = Order.create([{
-//     // name: 'Order',
-//     // properties: {
-//         order_id: 'string',
-//         user_id: 'string',
-//         drone_id: 'string',
-//         items: ['string'],
-//         destination_lat: 'number',
-//         destination_lon: 'number',
-//         completion_status: 'enum',
-//         time_placed: 'date',
-//     // }
-// }]);
-
-// describe('basic order test', () => {
-//     test('order exists and has correct types', ( ) => {
-//         expect(order1).not.toBeNull();
-//         expect(order1).toBeDefined();
-//         expect(order1.order_id).toBeDefined();
-//         expect(order1.user_id).toBeDefined();
-//         expect(order1.drone_id).toBeDefined();
-//         expect(order1.items).toBeDefined();
-//         expect(order1.destination_lat).toBeDefined();
-//         expect(order1.destination_lon).toBeDefined();
-//         expect(order1.completion_status).toBeDefined();
-//         expect(order1).toHavePropertyOfType('order_id', String);
-//         expect(order1).toHavePropertyOfType('user_id', String);
-//         expect(order1).toHavePropertyOfType('drone_id', String);
-//         // expect(order1).toHavePropertyOfType('destination_lat', Number);
-//         // expect(order1).toHavePropertyOfType('destination_lon', Number);
-//     });
-// });
+describe('basic order test', () => {
+    test('order exists and has correct types', ( ) => {
+        expect(order1).not.toBeNull();
+        expect(order1.order_id).not.toBeNull();
+        expect(order1.user_id).not.toBeNull();
+        expect(order1.drone_id).not.toBeNull();
+        expect(order1.items).not.toBeNull();
+        expect(order1.destination_lat).not.toBeNull();
+        expect(order1.destination_lon).not.toBeNull();
+        expect(order1.completion_status).not.toBeNull();
+        expect(order1).toHavePropertyOfType('order_id', String);
+        expect(order1).toHavePropertyOfType('user_id', String);
+        expect(order1).toHavePropertyOfType('drone_id', String);
+        expect(order1).toHavePropertyOfType('destination_lat', Number);
+        expect(order1).toHavePropertyOfType('destination_lon', Number);
+    });
+}); 
