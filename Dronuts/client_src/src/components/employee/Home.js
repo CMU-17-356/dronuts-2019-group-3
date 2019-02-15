9bea276ec70e8a7ead7591a41a9c61f90f7eb384
derekyyyan <dyyan@andrew.cmu.edu>
2019-02-15 15:04:25 -0500

    loopback 4 init

--- Dronuts/client_src/src/components/employee/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Home = () => (
-  <div>
-    <h1>Welcome employee, stop looking at this page and get to work immediately!</h1>
-  </div>
-)
-
-export default Home;
