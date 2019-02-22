8f1a68fd3658c1890901ebc4121b847c37ffd004
judyzhang25 <31805219+judyzhang25@users.noreply.github.com>
2019-02-22 00:03:50 -0500

    added mongodb as a datasource, changed the models to persistedmodel, exposed over REST, and using mongodb, and edited the models with correct datatypes -judy

--- Dronuts/common/models/drone.js
+++ Dronuts/common/models/drone.js
@@ -1,16 +1,6 @@
 'use strict';
-// var app = require('../../server/server');
 
 module.exports = function(Drone) {
     Drone.validatesPresenceOf('drone_id');
     Drone.validatesUniquenessOf('drone_id');
 };
-
-// var dronesSchema = {
-//     'drone_id': {
-//         'type': 'string',
-//         'required': true
-//     }
-// };
-
-// Model.extend('Drone', dronesSchema);
