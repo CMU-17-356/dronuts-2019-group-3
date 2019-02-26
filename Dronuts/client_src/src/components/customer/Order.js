f1e991bbb5cb38c7870e081e5b797851da406585
derekyyyan <dyyan@andrew.cmu.edu>
2019-02-26 01:30:29 -0500

    changed customer menu to fetch real data and updated cart to use session storage

--- Dronuts/client_src/src/components/customer/Order.js
+++ Dronuts/client_src/src/components/customer/Order.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 
 class Order extends Component {
+  constructor(){
+    super();
+    this.state = {cart: []};
+  }
+
+  componentDidMount(){
+    if(sessionStorage.getItem('cart')){
+      this.setState({cart: sessionStorage.getItem('cart')});
+    }
+  }
+
   render(){
-    return (
-      <div>
-        <h1>Sorry, there was an error with your order.</h1>
-      </div>
-    )
+    if (this.state.cart) {
+      return (
+        <div>
+          <p>Here is your cart but in raw JSON:</p>
+          <p>{this.state.cart}</p>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <h1>Sorry, there was an error with your order.</h1>
+        </div>
+      )
+    }
   }
 }
 
